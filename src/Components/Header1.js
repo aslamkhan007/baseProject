bbf668cdb12c3ebb5f668684a722e7ba8713b632
mohd aslam <makaslam786@hotmail.com>
2021-08-08 15:10:54 +0530

    final

--- src/Components/Header1.js
+++ src/Components/Header1.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 
 export default class Header1 extends Component {
     constructor(props){
-        alert('hi  c con');
+      //  alert('hi  c con');
         super(props);
-       // console.log(props)
+        //console.log(props)
     }
     render() {
        // debugger;
-       alert('hi c ren');
-       console.log(this.props.header)
+     //  alert('hi c ren');
+    // console.log(this.props.header)
+       console.log(this.props.header +'   ' +'  c')
         return (
             <div style={{border:"1px solid black"}}>
             
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,33 +1,17 @@
 import React, { Component } from "react";
 import Header1 from "./Header1";
+import ReactDom from "react-dom";
 
 export default class Login extends Component {
+  //abc = "hgh";
   constructor(props) {
     super(props);
     this.state = {
-      name: "wr",
-      login: "",
+      name: "",
+      login: ""
     };
-    const abc = "123";
-    alert('hi con');
   }
 
-  // handleChnage=(event)=> {
-  //     const {name,value}= event.target;
-  //   // console.log(event.target.value);
-  //   this.setState({[name]:value})
-
-  // }
-  // handleSubmit=(e)=>{
-  //   e.preventDefault();
-  //   <Header1
-  //   header={this.state.name}
-  //   />
-
-  // }
-  // clickevent=()=>{
-
-  // }
   hnadleChange = (e) => {
     e.preventDefault();
     this.setState({ name: e.target.value });
@@ -35,21 +19,58 @@ export default class Login extends Component {
     //console.log(e.target.value)
   };
 
-  handleOnclick = () => {
+
+    handleOnclick = async () => {
     // this.setState({ name: "ss" });
-    // this.setState({login:this.state.name})
-      this.abc = this.state.name;
-    //    console.log(this.abc);
+    // this.setState({ login: this.state.name });
+    //  this.abc = this.state.name;
+   await this.setState({ login: this.state.name });
+
+    // this.abc = "jkhkjs";
+    // console.log(`${this.abc} onclick pe set  `);
+
+    // console.log(`${this.state.name} onclick pe set  `);
+    // ReactDom.render(<Header1 header={this.state.name}/>,document.getElementById("root"));
+
+    console.log(`${this.state.login} onclick pe set  `);
+    ReactDom.render(<Header1 header={this.state.login}/>,document.getElementById("root"));
+
+
+  //ReactDom.render(<Header1 header= "aa"/>,document.getElementById("root"));
   };
+
+  // handleOnclick = () => {
+  //   // this.setState({ name: "ss" });
+  //   // this.setState({ login: this.state.name });
+  //   //  this.abc = this.state.name;
+  //   this.setState({ login: "hjhj" });
+
+  //   // this.abc = "jkhkjs";
+  //   // console.log(`${this.abc} onclick pe set  `);
+
+  //   // console.log(`${this.state.name} onclick pe set  `);
+  //   // ReactDom.render(<Header1 header={this.state.name}/>,document.getElementById("root"));
+
+  //   console.log(`${this.state.login} onclick pe set  `);
+  //   ReactDom.render(<Header1 header={this.state.login}/>,document.getElementById("root"));
+
+
+  // //ReactDom.render(<Header1 header= "aa"/>,document.getElementById("root"));
+  // };
+
+
+
   render() {
-    alert('hi ren');
+    // alert('hi ren');
+    console.log(`${this.abc} parent `);
     return (
       <div>
-        {/* <input
+        <input
           type="text"
           onChange={this.hnadleChange}
-        /> */}
-        
+          value={this.state.name}
+        />
+
         <button
           type="button"
           onClick={() => {
@@ -58,10 +79,12 @@ export default class Login extends Component {
         >
           button
         </button>
-        {/* <Header1 header={this.state.login}/> */}
+        <Header1 header={this.state.login} />
         {/* <Header1/> */}
-        <Header1 header={this.abc} />
-           {/* <Header1 header={this.state.name} /> */}
+        {/* <Header1 header={this.abc} /> */}
+        {/* <Header1 header={this.state.name} /> */}
+        {/* 
+        <Header1 header={this.abc} />   */}
       </div>
     );
   }
